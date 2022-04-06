e2d26df679fad0c97ae67210fd1983d04d0edfa3
Kzu <99527274+zokazu@users.noreply.github.com>
2022-04-06 21:15:55 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -184,9 +184,9 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/Rexashh/Xa-Userbot")
+    "https://github.com/zokazu/Kzu-Ubot")
 UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "Xa-Userbot")
+    "UPSTREAM_REPO_BRANCH", "Kzu-Ubot")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
